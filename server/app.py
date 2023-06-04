84097d40e0ba09bd8e86b85dbf5205afc4fb23ea
Hamed Fallah <hamedf@gmail.com>
2023-06-04 12:19:55 +0330

    add get flight details per each

--- server/app.py
+++ server/app.py
@@ -1,28 +1,44 @@
 import json
-from FlightRadar24.api import FlightRadar24API
+from FlightRadar24.api import FlightRadar24API, Flight
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return "Fids is Ready!"
 
 
-@app.route('/fids', methods=['GET'])
+@app.route("/fids", methods=["GET"])
 def get_value():
-    j = {"dest": "", "number": "", "aircraft_model": "", "airline_name": ""}
+    # j = {"dest": "", "number": "", "aircraft_model": "", "airline_name": ""}
     fr_api = FlightRadar24API()
-    flights = fr_api.get_flights(bounds="35.798,35.615,51.115,51.331")
+    req_params = [
+        "number",
+        "aircraft_model",
+        "airline_name",
+        "origin_airport_name",
+        "destination_airport_name",
+        "ground_speed",
+        "vertical_speed",
+        "time_details",
+    ]
+    # almost whole Tehran provine zone
+    zone_iran = "29.996625,29.185917,51.967958,53.478513"
+
+    flights = fr_api.get_flights(bounds=zone_iran)
     if len(flights) != 0:
-        j["dest"] = flights[0].destination_airport_name
-        j["number"] = flights[0].number
-        j["aircraft_model"] = flights[0].aircraft_model
-        j["airline_name"] = flights[0].airline_name
-    return json.dumps(j)
+        # iterate over first 3 flights only - for testing purpose
+        for flight in flights[0:2]:
+            details = fr_api.get_flight_details(flight.id)
+            flight.set_flight_details(details)
+
+    selected_flights = [
+        {key: getattr(flight, key) for key in req_params} for flight in flights[0:2]
+    ]
+    return json.dumps(selected_flights)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5050)
